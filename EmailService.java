20b281334d8488ffe8f0370983c88f88a2313f71
Tine <justinereplanpogi09@gmail.com>
2025-06-13 09:44:43 +0800

    Add files via upload

---
+++ EmailService.java
@@ -0,0 +1,39 @@
+package com.mycompany.ui;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.util.Properties;
+
+public class EmailService {
+
+    private final String username;
+    private final String password;
+    private final Session session;
+
+    public EmailService(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+    }
+
+    public void sendEmail(String toEmail, String subject, String messageText) throws MessagingException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(username));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+        message.setSubject(subject);
+        message.setText(messageText);
+        Transport.send(message);
+    }
+}
